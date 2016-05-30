a75ab2de48f9d3b7c0a5b592a5480ced5a651007
Tri Nguyen <mail@trimn.net>
2016-05-30 20:09:38 +0200

    Update tern to hook onto webmode

--- configs/tern.el
+++ configs/tern.el
@@ -1,8 +1,8 @@
 (add-to-list 'load-path (concat +emacs-dir+ "/libs/tern/emacs"))
 (autoload 'tern-mode "tern.el" nil t)
 
-(add-hook 'js3-mode-hook (lambda () (tern-mode t)))
-(add-hook 'js3-mode-hook (lambda () (auto-complete-mode t)))
+(add-hook 'web-mode-hook (lambda () (tern-mode t)))
+(add-hook 'web-mode-hook (lambda () (auto-complete-mode t)))
 
 (eval-after-load 'tern
   '(progn
