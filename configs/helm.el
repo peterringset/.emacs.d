4f44013a2ed8f948bd73e419c7a5ef164828aaa4
Tri Nguyen <mail@trimn.net>
2016-10-30 23:24:48 +0100

    Add helm.el

---
+++ configs/helm.el
@@ -0,0 +1,6 @@
+(require 'helm-config)
+(require 'helm-ls-git)
+
+(global-set-key (kbd "M-x") 'helm-M-x)
+(global-set-key (kbd "C-x C-f") 'helm-find-files)
+(global-set-key (kbd "C-x C-b") 'helm-buffers-list)
