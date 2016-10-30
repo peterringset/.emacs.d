f1e2c66d6c74b3eca52bae7831a8c51904068f78
Tri Nguyen <mail@trimn.net>
2016-10-30 23:25:25 +0100

    Add helm mode

--- configs/helm.el
+++ configs/helm.el
@@ -1,6 +1,8 @@
 (require 'helm-config)
 (require 'helm-ls-git)
 
+(helm-mode 1)
+
 (global-set-key (kbd "M-x") 'helm-M-x)
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
 (global-set-key (kbd "C-x C-b") 'helm-buffers-list)
