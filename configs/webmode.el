2c12ac347ea6868f868a6b698264d43c2ba1b184
Tri Nguyen <mail@trimn.net>
2016-10-31 01:44:08 +0100

    Add .jsx to webmode

--- configs/webmode.el
+++ configs/webmode.el
@@ -9,6 +9,7 @@
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.js?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.json?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.css?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.less?\\'" . web-mode))
