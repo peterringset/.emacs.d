feead0a926c92c02e56f16a3dd8c78ed716e1784
Tri Nguyen <mail@trimn.net>
2016-11-01 20:12:33 +0100

    Make emacs load dependencies

--- init.el
+++ init.el
@@ -1,29 +1,37 @@
 (require 'package)
-
-(add-to-list 'package-archives
-              '("melpa" . "https://melpa.org/packages/") t)
-(when (< emacs-major-version 24)
-     (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
-
+(setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")))
 (package-initialize)
 
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile (require 'use-package))
+(require 'diminish)
+(require 'bind-key)
+
+;; Directories
 (defconst +emacs-dir+ "~/.emacs.d")
 (defconst +emacs-conf-dir+ (concat +emacs-dir+ "/configs"))
 (defconst +emacs-snippets-dir+ (concat +emacs-dir+ "/snippets"))
 
 (add-to-list 'load-path (concat +emacs-dir+ "/includes"))
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
 
-(setenv "PATH" (concat (getenv "PATH") ":/usr/local/bin"))
-(setenv "NODE_PATH" "/usr/local/lib/node_modules")
+(global-hl-line-mode 1)
+(global-linum-mode 1)
 
 (defun load-config (f)
   (load-file (concat +emacs-conf-dir+ "/" f ".el")))
 
-(load-theme 'spacemacs-dark t)
+(setq mac-option-modifier nil
+      mac-command-modifier 'meta
+      shell-file-name "bash"
+      use-package-always-ensure t
+      x-select-enable-clipboard t)
 
-(global-hl-line-mode 1)
-(global-linum-mode 1)
-(setq shell-file-name "bash")
+(setq-default indent-tabs-mode nil)
 (setq-default line-spacing 5)
 
 (defadvice linum-update-window (around linum-dynamic activate)
@@ -32,28 +40,65 @@
          (linum-format (concat " %" (number-to-string w) "d ")))
     ad-do-it))
 
-(setq-default indent-tabs-mode nil)
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;; --------------------------------------
-(load-config "helm")
-(load-config "yasnippets")
-
-(load-config "css")
-(load-config "elm")
-(load-config "magit")
-(load-config "neotree")
-(load-config "tern")
-(load-config "webmode")
-
 ;; --------------------------------------
 
-(require 'nodejs-repl)
+(use-package spacemacs-theme
+  :init
+  (progn
+    (load-theme 'spacemacs-dark t)))
 
-(setq mac-option-modifier nil
-      mac-command-modifier 'meta
-      x-select-enable-clipboard t)
+(use-package magit
+  :bind (("C-x g" . magit-status)))
 
+(use-package helm
+  :bind (("C-x C-f" . helm-find-files)
+         ("C-x C-b" . helm-buffers-list)
+         ("M-x" . helm-M-x))
+  :diminish helm-mode
+  :init
+  (require 'helm-config)
+
+  :config
+  (helm-mode 1)
+
+  (use-package helm-ls-git
+    :bind (("C-x C-d" . helm-browse-project))))
+
+(use-package yasnippet
+  :diminish yas-minor-mode
+  :init
+  (setq yas-snippet-dirs '(+emacs-snippets-dir+))
+
+  :config
+  (yas-global-mode 1))
+
+(use-package elm-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.elm$" . elm-mode)))
+
+(use-package neotree
+  :bind (("C-x p" . neotree-toggle)))
+
+(use-package web-mode
+  :mode ("\\.html?\\'"
+         "\\.css?\\'"
+         "\\.less?\\'"
+         "\\.scss?\\'"
+         "\\.js?\\'"
+         "\\.jsx?\\'")
+
+  :init
+  (setq web-mode-markup-indent-offset 2
+        web-mode-css-indent-offset 2
+        web-mode-code-indent-offset 2))
+
+(use-package auto-complete
+  :init
+  (add-hook 'web-mode-hook 'auto-complete-mode))
+
+
+
+;; --------------------------------------
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -61,11 +106,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(column-number-mode t)
- '(helm-mode t)
  '(magit-commit-arguments (quote ("--gpg-sign=999ABCF36AE3B637")))
  '(package-selected-packages
    (quote
-    (yasnippet helm-ls-git helm web-mode sublime-themes spacemacs-theme spacegray-theme neotree markdown-mode magit less-css-mode jsx-mode js3-mode js2-mode elm-mode dash-functional ac-math ac-html)))
+    (tern-auto-complete tern yasnippet helm-ls-git helm web-mode sublime-themes spacemacs-theme spacegray-theme neotree markdown-mode magit less-css-mode jsx-mode js3-mode js2-mode elm-mode dash-functional ac-math ac-html)))
  '(scroll-bar-mode nil)
  '(standard-indent 2)
  '(tool-bar-mode nil))
