fc0bbf6b6b4c19d24808d8ead8a1c3eb665d6eed
Tri Nguyen <mail@trimn.net>
2016-11-02 17:00:27 +0100

    Remove old custom font set

--- init.el
+++ init.el
@@ -148,6 +148,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- (if (eq system-type 'darwin)
-     '(default ((t (:height 120 :family "Menlo"))))
-   ))
+)
