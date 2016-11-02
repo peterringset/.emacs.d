ddbac742a7f0e863ee62b09c8d1244ff3707b093
Tri Nguyen <tri.nguyen@sparebank1.no>
2016-11-02 07:20:32 +0100

    Add checks for darwin when setting font

--- init.el
+++ init.el
@@ -147,4 +147,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:height 120 :family "Menlo")))))
+ (if (eq system-type 'darwin)
+     '(default ((t (:height 120 :family "Menlo"))))
+   ))
