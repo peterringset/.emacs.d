a6cc02e8190c0ca4c2898efdfbb4a9bb74bdf06e
Tri Nguyen <mail@trimn.net>
2016-11-04 00:46:22 +0100

    Update js2-mode setup

--- init.el
+++ init.el
@@ -217,6 +217,14 @@
 (use-package js2-mode
   :mode "\\.js$"
   :init
+  (setq-default js2-show-parse-errors nil
+                js2-strict-missing-semi-warning nil
+                js2-strict-inconsistent-return-warning nil
+                js2-strict-var-hides-function-arg-warning nil
+                js2-strict-cond-assign-warning nil
+                js2-strict-var-redeclaration-warning nil
+                js2-strict-trailing-comma-warning t)
+
   (setq js2-highlight-level 3)
   (setq-default js2-basic-offset 2)
   (setq js-indent-level 2)
