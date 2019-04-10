a749839187b70fa430c295e545d5771f130de352
Peter Ringset <peter.ringset@eggsdesign.com>
2019-04-10 13:12:35 +0200

    Use custom background color

--- init.el
+++ init.el
@@ -201,7 +201,8 @@
           doom-themes-enable-italic t)      ; if nil, italics are universally disabled
     (doom-themes-visual-bell-config))
   :init
-  (load-theme 'doom-vibrant t))
+  (load-theme 'doom-vibrant t)
+  (set-background-color "#1E1E1E"))
 
 (use-package magit
   :commands magit-status
