58e8e15a5b6c252e2b2cb6f010e4abd612e31083
Tri Nguyen <mail@trimn.net>
2020-08-29 19:14:14 +0200

    Always open magit to the right

--- lisp/t-magit.el
+++ lisp/t-magit.el
@@ -4,7 +4,15 @@
 
 (use-package magit
   :commands magit-status
-  :bind (("C-x g" . magit-status)))
+  :bind (("C-x g" . magit-status))
+  :config
+  (add-to-list 'display-buffer-alist
+               `(,(rx bos "magit: ")
+                 (display-buffer-reuse-window
+                  display-buffer-in-side-window)
+                 (side            . right)
+                 (reusable-frames . visible)
+                 (window-width    . 0.50))))
 
 (provide 't-magit)
 ;;; t-magit.el ends here
