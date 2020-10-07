466f194de89a0ce85a3bb18c6c6192872943d76d
Peter Ringset <peter.ringset@eggsdesign.com>
2020-10-07 19:44:08 +0200

    Enable pull request shortcut for magit

--- lisp/t-magit.el
+++ lisp/t-magit.el
@@ -6,5 +6,23 @@
   :commands magit-status
   :bind (("C-x g" . magit-status)))
 
+(setq browse-url-browser-function 'browse-url-default-macosx-browser)
+
+(defun endless/visit-pull-request-url ()
+  "Visit the current branch's PR on Github."
+  (interactive)
+  (browse-url
+   (format "https://github.com/%s/pull/new/%s"
+           (replace-regexp-in-string
+            "\\`.+github\\.com:\\(.+\\)\\.git\\'" "\\1"
+            (magit-get "remote"
+                       (magit-get-push-remote)
+                       "url"))
+           (magit-get-current-branch))))
+
+(eval-after-load 'magit
+  '(define-key magit-mode-map "v"
+     #'endless/visit-pull-request-url))
+
 (provide 't-magit)
 ;;; t-magit.el ends here
