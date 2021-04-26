6a14ef342adf4e719f09fcaf3d3a2a806cd8bba0
Peter Ringset <peter.ringset@eggsdesign.com>
2021-04-26 09:43:56 +0200

    Set custom background color

--- pr.el
+++ pr.el
@@ -5,4 +5,17 @@
 
 ;;; Code:
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:background "#1E1E1E")))))
+
 ;;; pr.el ends here
